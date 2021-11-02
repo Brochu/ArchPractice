2ded24076b190e2e144f38aba28c5e1ed3615eec
Alex <alexbrochu1@gmail.com>
2021-11-02 02:45:24 -0400

    started structure for singleton test

---
+++ second/singleton.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string>
+
+class MyManager
+{
+    public:
+        std::string value;
+
+    private:
+        MyManager(){};
+        ~MyManager(){};
+
+        MyManager(MyManager&) = delete;
+        void operator=(MyManager&) = delete;
+
+        //MyManager(MyManager&&) = delete;
+        //void operator=(MyManager&&) = delete;
+};
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
