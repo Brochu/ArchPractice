0319489e37bdbfb0a632e7d95a1e462cb729790d
Alex <alexbrochu1@gmail.com>
2021-11-03 01:21:05 -0400

    Finished first pass on singleton pattern, missing private dtor ...

--- second/singleton.cpp
+++ second/singleton.cpp
@@ -1,20 +1,37 @@
+#include <memory>
 #include <stdio.h>
 #include <string>
 
 class MyManager
 {
     public:
-        std::string value;
+        static MyManager& GetInstance()
+        {
+            static std::unique_ptr<MyManager> instance(new MyManager());
+
+            return *instance;
+        }
+
+        std::string GetValue() const { return _val; }
+        void SetValue(std::string& newVal) { _val = std::string(newVal); }
 
     private:
         MyManager(){};
-        ~MyManager(){};
 
         MyManager(MyManager&) = delete;
         void operator=(MyManager&) = delete;
+
+        std::string _val;
 };
 
 int main(int argc, char** argv)
 {
+    printf("[MAIN] First call to `GetInstance` will create the instance.\n");
+    printf("[MAIN] Current value in the Manager = %s.\n", MyManager::GetInstance().GetValue().c_str());
+
+    std::string newVal = "Hello, world!!";
+    MyManager::GetInstance().SetValue(newVal);
+    printf("[MAIN] New value in the Manager = %s.\n", MyManager::GetInstance().GetValue().c_str());
+
     return 0;
 }
