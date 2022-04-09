fdfe35b26c78a4052c578914c6b43b664d335a09
Alex Brochu <alexbrochu1@gmail.com>
2022-04-08 21:37:54 -0400

    Implement another version of a sorting program. InterviewPrep. Will try merge sort as well

--- InterviewPrep/sorting/main.cpp
+++ InterviewPrep/sorting/main.cpp
@@ -1,56 +1,57 @@
 #include <stdio.h>
+#include <string.h>
 #include <vector>
 
-void Debug(int *arr, int size)
+void Debug(const std::vector<int>& nums)
 {
-    printf("Array: ");
-    for (int i = 0; i < size; i++)
+    for(const int n : nums)
     {
-        printf("%i ", arr[i]);
+        printf("%i ", n);
     }
     printf("\n");
 }
 
-int Partition(int *arr, int l, int h)
+// Quicksort START
+int HoarePartition(int *arr, int lo, int hi)
 {
-    int pivot = arr[(h + l) / 2];
-
-    int i = l - 1, j = h + 1;
-
-    while(true)
-    {
-        do i++; while(arr[i] < pivot);
-        do j--; while(arr[j] > pivot);
-
-        if (i >= j) return j;
-
-        std::swap(arr[i], arr[j]);
-    }
-
     return -1;
 }
 
-void Quicksort(int *arr, int l, int h)
+void Quicksort(int *arr, int lo, int hi)
 {
-    // Base case, we can stop recursion here
-    if (l >= h) return;
-
-    int p = Partition(arr, l, h);
-
-    Quicksort(arr, l, p);
-    Quicksort(arr, p + 1, h);
 }
+// Quicksort END
 
+
+// Merge sort START
+// Merge sort END
 int main(int argc, char **argv)
 {
-    std::vector<int> array { 32, 56, 78, 92, 12, 18, 0, 23, 11, 45, 63 };
+    std::vector<int> nums { 65, 23, 10, 89, 99, 72, 52, 12, 45, 0, 16, 79, 81 };
 
-    printf("[MAIN] Before quicksort\n");
-    Debug(array.data(), array.size());
-    Quicksort(array.data(), 0, array.size() - 1);
+    printf("[MAIN] Welcome to sorting\n");
+    printf("[MAIN] Sorting the following list\n");
+    Debug(nums);
 
-    printf("[MAIN] After quicksort\n");
-    Debug(array.data(), array.size());
+    if (argc == 2)
+    {
+        if (strcmp(argv[1], "Quick") == 0)
+        {
+            Quicksort(nums.data(), 0, nums.size() - 1);
+        }
+        else if (strcmp(argv[1], "Merge") == 0)
+        {
+            //TODO: Implement merge sort
+        }
+    }
+    else
+    {
+        printf("[MAIN] Please only specify sort type (Quick, Merge)\n");
+    }
+
+    printf("[MAIN] ... DONE ...\n");
+    printf("[MAIN] Here is the sorted list\n");
+    Debug(nums);
 
     return 0;
 }
