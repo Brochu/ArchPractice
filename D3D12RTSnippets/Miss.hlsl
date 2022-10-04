2c40ca481642201478a25f0a202b43d9ead8106b
Alex Brochu <alexbrochu1@gmail.com>
2022-10-04 15:11:56 -0400

    Better version of the miss shader, ramp based on y axis

--- D3D12RTSnippets/Miss.hlsl
+++ D3D12RTSnippets/Miss.hlsl
@@ -3,5 +3,14 @@
 [shader("miss")]
 void Miss(inout HitInfo payload : SV_RayPayload)
 {
-    payload.colorAndDistance = float4(0.2f, 0.2f, 0.8f, -1.f);
+    //Miss Shader
+    //To slightly differentiate the raster and the raytracing, we will add a simple ramp color background by
+    //modifying the `Miss` function: we simply obtain the
+    //coordinates of the currently rendered pixel using and use them to compute a linear gradient:
+
+    uint2 launchIndex = DispatchRaysIndex().xy;
+    float2 dims = float2(DispatchRaysDimensions().xy);
+    float ramp = launchIndex.y / dims.y;
+
+    payload.colorAndDistance = float4(0.0f, 0.2f, 0.7f - 0.3f*ramp, -1.0f);
 }
