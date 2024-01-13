2b10f6176bd7b8df70ee49706fb521e7997880ae
Brochu <alexbrochu1@gmail.com>
2024-01-12 23:25:08 -0500

    Some updates, lost track ..

--- debugsstr/main.cpp
+++ debugsstr/main.cpp
@@ -9,17 +9,16 @@ int main(int argc, char** argv)
     printf("Starting Test ...\n");
 
     const char* values = "vn 16.6 14.4 15.5";
-    std::stringstream ss;
-    //std::stringstream ss(values);
+    std::stringstream ss(values);
 
-    //std::string type;
-    //ss >> type;
+    std::string type;
+    ss >> type;
 
-    //float f0, f1, f2;
-    //ss >> f0;
-    //ss >> f1;
-    //ss >> f2;
-    //printf("Parsed:\n%s:\n%f\n%f\n%f\n", type.c_str(), f0, f1, f2);
+    float f0, f1, f2;
+    ss >> f0;
+    ss >> f1;
+    ss >> f2;
+    printf("Parsed:\n%s:\n%f\n%f\n%f\n", type.c_str(), f0, f1, f2);
 
     printf("Test Over ...\n");
     return 0;
